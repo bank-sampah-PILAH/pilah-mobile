6253d3c40e063ec6c43f7913a11920d0e578f537
Fideligo <mrafighalibf@gmail.com>
2026-06-18 01:58:13 +0700

    fix: add named GetIt initialization for AppStorage in main entry points

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,18 @@
-﻿import 'package:pilah_mobile/app.dart';
+import 'package:pilah_mobile/app.dart';
 import 'package:pilah_mobile/services/di.dart';
 import 'package:flutter/material.dart';
 
 import 'core/constants/app_key.dart';
 
+import 'package:pilah_mobile/core/storage/app_storage.dart';
+import 'package:pilah_mobile/core/client/network_utils.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies(environment: AppKey.prodEnv);
+  
+  await di.get<AppStorage>(instanceName: 'shared_preferences').init();
+  await di<NetworkUtils>().init();
+
   runApp(const App());
 }
