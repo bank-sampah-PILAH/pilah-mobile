6ad944bedc9189a8c2d3486e3a6a6dfbde8989c6
Fideligo <mrafighalibf@gmail.com>
2026-06-28 19:12:55 +0700

    fix: add missing firebase initialization before google sign in

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,6 +2,7 @@ import 'package:pilah_mobile/app.dart';
 import 'package:pilah_mobile/services/di.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'core/constants/app_key.dart';
 
@@ -14,6 +15,9 @@ Future<void> main() async {
   
   await di.get<AppStorage>(instanceName: 'shared_preferences').init();
   await di<NetworkUtils>().init();
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
 
   // Initialize Google Sign-In (required for v7+)
   await GoogleSignIn.instance.initialize();
