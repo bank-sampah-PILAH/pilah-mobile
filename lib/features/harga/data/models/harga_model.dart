f0196df4b368f313b429755fa3a101b3a3e2a6a7
Fideligo <mrafighalibf@gmail.com>
2026-07-05 12:51:08 +0700

    feat(harga): setup remote data source and implement manual json serialization for django

--- lib/features/harga/data/models/harga_model.dart
+++ lib/features/harga/data/models/harga_model.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:pilah_mobile/features/harga/domain/entities/harga_entity.dart';
 
-part 'harga_model.g.dart';
-
-@JsonSerializable(converters: [ColorConverter(), IconDataConverter()])
 class HargaModel extends HargaEntity {
   HargaModel({
     required super.id,
@@ -19,28 +15,62 @@ class HargaModel extends HargaEntity {
     required super.isActive,
   });
 
-  factory HargaModel.fromJson(Map<String, dynamic> json) => _$HargaModelFromJson(json);
+  factory HargaModel.fromJson(Map<String, dynamic> json) {
+    final String priceStr = json['harga_per_kg']?.toString() ?? '0';
+    final int price = double.tryParse(priceStr)?.toInt() ?? 0;
+    final String category = json['kategori']?.toString() ?? 'dll';
+    
+    IconData icon = Icons.recycling;
+    Color iconColor = Colors.green;
+    String badgeText = 'Anorganik';
+    
+    switch (category.toLowerCase()) {
+      case 'kertas':
+        icon = Icons.description;
+        iconColor = Colors.blue;
+        break;
+      case 'plastik':
+        icon = Icons.local_drink;
+        iconColor = Colors.green;
+        break;
+      case 'logam':
+        icon = Icons.hardware;
+        iconColor = Colors.grey;
+        break;
+      case 'kaca':
+        icon = Icons.wine_bar;
+        iconColor = Colors.teal;
+        break;
+      case 'organik':
+        icon = Icons.eco;
+        iconColor = Colors.lightGreen;
+        badgeText = 'Organik';
+        break;
+    }
 
-  Map<String, dynamic> toJson() => _$HargaModelToJson(this);
-}
+    return HargaModel(
+      id: json['id'] as String? ?? '',
+      name: json['nama_sampah'] as String? ?? '',
+      price: price,
+      priceFormatted: 'Rp $price',
+      category: category,
+      subtitle: json['deskripsi'] as String? ?? '',
+      badgeText: badgeText,
+      icon: icon,
+      iconColor: iconColor,
+      isActive: json['is_active'] as bool? ?? true,
+    );
+  }
 
-class ColorConverter implements JsonConverter<Color, int> {
-  const ColorConverter();
-
-  @override
-  Color fromJson(int json) => Color(json);
-
-  @override
-  // ignore: deprecated_member_use
-  int toJson(Color object) => object.value;
-}
-
-class IconDataConverter implements JsonConverter<IconData, int> {
-  const IconDataConverter();
-
-  @override
-  IconData fromJson(int json) => IconData(json, fontFamily: 'MaterialIcons');
-
-  @override
-  int toJson(IconData object) => object.codePoint;
+  Map<String, dynamic> toJson() {
+    return {
+      if (id.isNotEmpty && !id.startsWith('JS')) 'id': id,
+      'kode': 'JS-${DateTime.now().millisecondsSinceEpoch}', // Required field in backend
+      'nama_sampah': name,
+      'kategori': category.toLowerCase(),
+      'deskripsi': subtitle,
+      'harga_per_kg': price.toString(),
+      'is_active': isActive,
+    };
+  }
 }
