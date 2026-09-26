ac5d99b5f41e37d1942701eb65e7ae36a9398558
TrayMachi <tristanagraa@gmail.com>
2026-09-26 10:23:17 +0700

    fix(auth): sign out before handoff account switch

--- test/features/onboarding/presentation/pages/role_handoff_page_test.dart
+++ test/features/onboarding/presentation/pages/role_handoff_page_test.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart'
+    as google_sign_in;
 import 'package:mocktail/mocktail.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:pilah_mobile/features/authentication/domain/model/auth.dart';
 import 'package:pilah_mobile/features/authentication/presentation/blocs/authentication_bloc.dart';
 import 'package:pilah_mobile/features/authentication/presentation/blocs/authentication_events.dart';
@@ -17,6 +20,10 @@ import 'package:pilah_mobile/features/onboarding/presentation/pages/role_handoff
 class _MockAuthBloc extends MockBloc<AuthenticationEvent, AuthenticationStates>
     implements AuthenticationBloc {}
 
+class _MockGoogleSignInPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements google_sign_in.GoogleSignInPlatform {}
+
 class _FakeAuthEvent extends Fake implements AuthenticationEvent {}
 
 void main() {
@@ -25,6 +32,18 @@ void main() {
   testWidgets('lets a role handoff user sign out and return to login',
       (tester) async {
     final auth = _MockAuthBloc();
+    final originalGoogleSignInPlatform =
+        google_sign_in.GoogleSignInPlatform.instance;
+    final googleSignInPlatform = _MockGoogleSignInPlatform();
+    google_sign_in.GoogleSignInPlatform.instance = googleSignInPlatform;
+    final signOutCompleted = Completer<void>();
+    when(() =>
+            googleSignInPlatform.signOut(const google_sign_in.SignOutParams()))
+        .thenAnswer((_) => signOutCompleted.future);
+    addTearDown(() {
+      google_sign_in.GoogleSignInPlatform.instance =
+          originalGoogleSignInPlatform;
+    });
     final states = StreamController<AuthenticationStates>();
     addTearDown(states.close);
     addTearDown(auth.close);
@@ -71,6 +90,16 @@ void main() {
     await tester.pump();
 
     await tester.tap(find.text('Keluar dan ganti akun'));
+    await tester.pump();
+    verifyNever(() => auth.add(any<AuthenticationEvent>(
+          that: isA<LogoutRequested>(),
+        )));
+
+    signOutCompleted.complete();
+    await tester.pumpAndSettle();
+    verify(() =>
+            googleSignInPlatform.signOut(const google_sign_in.SignOutParams()))
+        .called(1);
     verify(() => auth.add(any<AuthenticationEvent>(
           that: isA<LogoutRequested>(),
         ))).called(1);
